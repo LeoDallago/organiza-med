7262e202dd7ca6b1e40e2ca6303d8a29e979f62d
LeoDallago <leodallago@outlook.com>
2024-11-23 15:13:15 -0300

    Back-End: Servico (Autenticação) implementado

---
+++ Server/OrganizaMed.Aplicacao/ModuloAutenticacao/ServicoAutenticacao.cs
@@ -0,0 +1,51 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using OrganizaMed.Dominio.ModuloAutenticacao;
+
+namespace OrganizaMed.Aplicacao.ModuloAutenticacao;
+
+public class ServicoAutenticacao(UserManager<Usuario> userManager, SignInManager<Usuario> signInManager)
+{
+    private readonly UserManager<Usuario> _userManager = userManager;
+    private readonly SignInManager<Usuario> _signInManager = signInManager;
+
+    public async Task<Result<Usuario>> RegistrarAsync(Usuario usuario, string senha)
+    {
+        var usuarioResult = await _userManager.CreateAsync(usuario, senha);
+        
+        if (!usuarioResult.Succeeded)
+        {
+            return Result
+                .Fail(usuarioResult
+                    .Errors
+                    .Select(failure => new Error(failure.Description))
+                );
+        }
+
+        return Result.Ok(usuario);
+    }
+
+    public async Task<Result<Usuario>> Autenticar(string login, string senha)
+    {
+        var loginResult = await _signInManager.PasswordSignInAsync(login, senha, false, true);
+        var erros = new List<Error>();
+        if (loginResult.IsLockedOut)
+            erros.Add(new Error("O acesso para este usuário foi bloqueado"));
+        if (loginResult.IsNotAllowed)
+            erros.Add(new Error("O acesso para este usuário é permitido"));
+        if (!loginResult.Succeeded)
+            erros.Add(new Error("O login ou a senha estão incorretas"));
+        if (erros.Count > 0)
+            return Result.Fail(erros);
+        var usuario = await _userManager.FindByNameAsync(login);
+        if (usuario == null)
+            return Result.Fail("Não foi possível encontrar o usuário");
+        return Result.Ok(usuario);
+    }
+
+    public async Task<Result<Usuario>> Sair()
+    {
+        await _signInManager.SignOutAsync();
+        return Result.Ok();
+    }
+}
