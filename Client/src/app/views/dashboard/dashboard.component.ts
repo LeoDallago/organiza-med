f99046ae82c0b818f1a16862cf23dd51fedc3d8b
LeoDallago <leodallago@outlook.com>
2024-12-02 13:59:46 -0300

    Front-End: Tela inicial implementada.

--- Client/src/app/views/dashboard/dashboard.component.ts
+++ Client/src/app/views/dashboard/dashboard.component.ts
@@ -1,14 +1,47 @@
-import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from '@angular/core';
+import { ListarAtendimentoViewModel } from '../atendimento/models/atendimento.model';
+import { AtendimentoService } from '../atendimento/services/atendimento.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgForOf, NgIf, AsyncPipe],
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
-  constructor(private toastr: ToastrService) { }
+  atendimentos?: Observable<ListarAtendimentoViewModel[]>
+  public mensagem: string = ''
+
+  public diaHoje = new Date()
+  public dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+  public dataFormatada = new Intl.DateTimeFormat('pt-BR', this.dateOptions).format(this.diaHoje)
+
+  constructor(private atendimentosService: AtendimentoService) { }
+
+  ngOnInit(): void {
+    this.atendimentos = this.atendimentosService.selecionarTodos();
+  }
+
+  periodoMensagem() {
+    var hora = this.diaHoje.getHours()
+
+    if (hora > 12 && hora < 18) {
+      this.mensagem = 'Boa tarde'
+    } else if (hora >= 18 && hora < 24) {
+      this.mensagem = 'Boa Noite'
+    } else {
+      this.mensagem = 'Bom dia'
+    }
+
+    return this.mensagem
+  }
 }
 
